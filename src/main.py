bdd2b3254c4cea45213eed326d1792f8fe74deec
wasiq abdullah <wasiqabdullah222@gmail.com>
2023-12-07 19:36:28 +0500

    Created piepline

--- src/main.py
+++ src/main.py
@@ -1,3 +1,14 @@
 from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to my Custom Log")
+STAGE_NAME = 'Data Ingestion Stage'
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx========x")
+
+    except Exception as e:
+        raise e
